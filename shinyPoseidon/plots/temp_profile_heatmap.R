5272f67fe02b21a7912464971dc92b1e6f2faf14
Atul Ingle <ingle.atul@gmail.com>
2019-08-19 19:35:12 -0500

    xticklabels on temperature heat map
    We now show dates as the x tick labels. (Dates will change
    for each year.)

--- shinyPoseidon/plots/temp_profile_heatmap.R
+++ shinyPoseidon/plots/temp_profile_heatmap.R
@@ -13,11 +13,15 @@ unique.dates <- decimal_date(unique(heatmap.data$sample.date))
 heatmap.data$sample.date <- decimal_date(heatmap.data$sample.date)
 heatmap.data <- interp(x = heatmap.data$sample.date, y = heatmap.data$neg.depth, z = heatmap.data$temp.C, duplicate = "strip")
 
-par(mar = c(3,3,2,.5))
+par(mar = c(4.5,3,2,0.5))
 
 image(heatmap.data, axes = F, col = sequential_hcl(n = 20, palette = "plasma"))
-axis(side = 1,lwd = 2)
-axis(side = 1, at= unique.dates, col = "red", labels=F)#, labels = unique.dates)
+
+day.choices <- unique(ysi$sample.date[index])
+day.choices <- paste(lubridate::month(x = day.choices, label = TRUE, abbr = TRUE), day(x = day.choices))
+#day.choices[seq(2,length(day.choices)-1)]="" # only show first and last day names
+
+axis(side = 1, at= unique.dates, col = "black", labels=as.character(day.choices),las=2)#, labels = unique.dates)
 # axis(side = 1, tick = T, line = 0, at = 2015 + yday(date.options)/365, srt = 90)
      #at = 2015 + c(176, 182, 195, 204, 217, 222, 266, 278, 293, 310)/365,
      #label = c("6-24", "6-30", "7-13", "7-22", "8-04", "9-22", "10-04", "10-19", "11-05")
