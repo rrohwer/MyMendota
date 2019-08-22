576a18485d55810e90ca4e5fba5b8f7e9ef75432
Robin Rohwer <Robin.Rohwer@gmail.com>
2019-08-21 21:36:58 -0500

    fixed another minor bug- hadnt updated location of F lake surface label.

--- shinyPoseidon/plots/temp_profile_daily.R
+++ shinyPoseidon/plots/temp_profile_daily.R
@@ -32,13 +32,7 @@ if(input$TempPref == "Celcius"){
   x.ax.locs <- seq(from = 32, to = 77, by = 5)
 }  
   
-
-
-
-
-
 par(mar = c(3,3,3,.5))
-
 
 plot(x = my.temp, y = my.ysi$neg.depth, type = "n", ylim = y.lim, xlim = x.lim, axes = F, ann = F)
 lines(x = t, y = y, col = adjustcolor("blue",.5), lwd = 5, xpd = NA)
@@ -49,7 +43,7 @@ axis(side = 1, at = x.ax.locs, tick = 0, labels = T, line = -.5)
 mtext(text = ax.lab, side = 1, line = 1.75, outer = F)
 axis(side = 2, at = seq(from = -20, to = 0, by = 4), labels = F)
 axis(side = 2, at = seq(from = -20, to = 0, by = 4), labels = seq(from = -20, to = 0, by = 4) * -1, tick = 0, line = -.25, las = 2)
-text(x = 2, y = 2, labels="(Lake Surface: Depth = 0)", cex = 0.75, xpd = NA)
+text(x = x.lim[1], y = 2, labels="(Lake Surface: Depth = 0)", cex = 0.75, xpd = NA)
 mtext(text = "Depth (m)", side = 2, line = 2, outer = F)
 mtext(text = paste(input$slider.day, input$chosen.year, sep = ", "), side = 3, line = 1.7, outer = F, cex = 1.5)
 
