c2e13504dbf7987b75d2c85faaf4b74b240f6abc
Robin Rohwer <Robin.Rohwer@gmail.com>
2019-08-21 20:48:11 -0500

    fixed DO daily plot so that waves are drawn with lines() instead of overwriting the plot. also they are wave shaped instead of ripple shaped now. cuz mendota a big lake.

--- shinyPoseidon/plots/DO_profile_daily.R
+++ shinyPoseidon/plots/DO_profile_daily.R
@@ -13,13 +13,14 @@ my.ysi <- ysi[index, ]
 y.lim <- c(-20,0)
 x.lim <- c(0, max(ysi$DO.mg.L, na.rm = TRUE))
 
-t=seq(0,21,0.5)
-y=0.7*sin(t)
+t <- seq(from = 0, to = 21, length.out = 100)
+y <- -abs(x = 0.7 * sin(t))
+y <- y - min(y)
 #plot(t,y, ylim = y.lim, type="l", xlab="time", ylab="Sine wave")
 
-par(mar = c(3,3,2,.5))
+par(mar = c(3,3,3,.5))
 plot(x = my.ysi$DO.mg.L, y = my.ysi$neg.depth, type = "n", ylim = y.lim, xlim = x.lim, axes = F, ann = F)
-plot(t,y, ylim = c(-20,1), xlim=x.lim, type="l", axes=F, ann=F,col='blue',lty=2,lwd=2)
+lines(x = t, y = y, col= adjustcolor('blue', .5), lwd = 5, xpd = NA)
 points(x = my.ysi$DO.mg.L, y = my.ysi$neg.depth, pch = 21, col = "black", bg = adjustcolor("black",.5))
 axis(side = 1, at = x.lim, labels = F, lwd.ticks = 0)
 axis(side = 1, at = seq(from = 0, to = 20, by = 5), labels = F)
@@ -27,9 +28,9 @@ axis(side = 1, at = seq(from = 0, to = 20, by = 5), tick = 0, labels = T, line =
 mtext(text = "Dissolved Oxygen (mg/L)", side = 1, line = 1.75, outer = F)
 axis(side = 2, at = seq(from = -20, to = 0, by = 4), labels = F)
 axis(side = 2, at = seq(from = -20, to = 0, by = 4), labels = seq(from = -20, to = 0, by = 4) * -1, tick = 0, line = -.25, las = 2)
-text(1.5,1.1,labels="[Lake Surface: Depth=0]",cex=0.75)
+text(x = 1.5, y = 2, labels ="(Lake Surface: Depth = 0)", cex = 0.75, xpd = NA)
 mtext(text = "Depth (m)", side = 2, line = 2, outer = F)
-mtext(text = paste(input$slider.day, input$chosen.year, sep = ", "), side = 3, line = 0, outer = F, cex = 1.5)
+mtext(text = paste(input$slider.day, input$chosen.year, sep = ", "), side = 3, line = 1.7, outer = F, cex = 1.5)
 
 
 
